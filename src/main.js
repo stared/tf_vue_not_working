7a911a4e70da03d65587bcade2bdc1a5d96b483d
Piotr Migdał <pmigdal@gmail.com>
2018-08-13 16:15:59 +0200

    added tfjs and does not work in Chrome

--- src/main.js
+++ src/main.js
@@ -1,8 +1,27 @@
-import Vue from 'vue'
-import App from './App.vue'
+// import Vue from 'vue'
+// import App from './App.vue'
+//
+// Vue.config.productionTip = false
+//
+// new Vue({
+//   render: h => h(App)
+// }).$mount('#app')
 
-Vue.config.productionTip = false
+import * as tf from '@tensorflow/tfjs';
 
-new Vue({
-  render: h => h(App)
-}).$mount('#app')
+// Define a model for linear regression.
+const model = tf.sequential();
+model.add(tf.layers.dense({units: 1, inputShape: [1]}));
+
+// Prepare the model for training: Specify the loss and the optimizer.
+model.compile({loss: 'meanSquaredError', optimizer: 'sgd'});
+
+// Generate some synthetic data for training.
+const xs = tf.tensor2d([1, 2, 3, 4], [4, 1]);
+const ys = tf.tensor2d([1, 3, 5, 7], [4, 1]);
+
+// Train the model using the data.
+model.fit(xs, ys, {epochs: 10}).then(() => {
+  // Use the model to do inference on a data point the model hasn't seen before:
+  model.predict(tf.tensor2d([5], [1, 1])).print();
+});
